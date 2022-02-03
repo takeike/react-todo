f771e5c6253df1d72de894bc2a81a9fe4bfd3d5c
takeike <takemasa.ikeda@konicaminolta.com>
2022-02-03 20:39:53 +0900

    51 create basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="ToDoを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTodo</p>
+        <ul>
+          <div className="list-row">
+            <li>ああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div className="list-row">
+            <li>いいいい</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTodo</p>
+        <ul>
+          <div className="list-row">
+            <li>うううう</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
